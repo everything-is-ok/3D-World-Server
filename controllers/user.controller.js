4f018c6aab33ab66dc94e877cb5a5dfee605184e
minwooKim <vvee1253@gmail.com>
2021-04-16 22:35:28 +0900

    ADD error handle to findOrCreate function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,9 +15,10 @@ async function postLogin(req, res, next) {
   const { userData } = req.body;
 
   // TODO: 유저데이터로 find-or-create시, 유저의 데이터 중 하나가 달라지면 찾는가 생성하는가 등의 문제 찾고 해결하기.
-  const { doc: user } = await User.findOrCreate(userData);
-
   try {
+    // TODO: error 1. findOrCreate/ error2: jwt sign/ 분기처리?
+    const { doc: user } = await User.findOrCreate(userData);
+
     const accessToken = jwt.sign({
       id: user._id,
     }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3h" });
