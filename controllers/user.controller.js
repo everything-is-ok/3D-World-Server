31b6d4e30981069f64c2af7b3fb01c0480c6bb39
minwooKim <vvee1253@gmail.com>
2021-04-16 21:25:36 +0900

    ADD postLogin function in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,4 +8,43 @@ function getUser(req, res) {
   });
 }
 
+function postLogin(req, res, next) {
+  const { userData } = req.body;
+
+  // TODO: 유저데이터로 find-or-create시, 유저의 데이터 중 하나가 달라지면 찾는가 생성하는가 등의 문제 찾고 해결하기.
+  const { doc: user } = await User.findOrCreate(userData);
+
+  try {
+    const accessToken = jwt.sign({
+      id: user._id,
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3h" });
+
+    res.cookie("authorization", `bearer ${accessToken}`);
+    res.json({ data: user });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+
+
+}
+
 exports.getUser = getUser;
+exports.postLogin = postLogin;
+
+// const { displayName, email, photoURL } = req.body;
+
+//   const user = {
+//     email,
+//     photoURL,
+//     displayName,
+//   };
+
+//   const { doc: userInDB } = await User.findOrCreate(user);
+
+//   const accessToken = jwt.sign(
+//     { id: userInDB._id, displayName, photoURL },
+//     process.env.ACCESS_TOKEN_SECRET,
+//     { expiresIn: 10000 }
+//   );
+
+//   res.status(201).json(accessToken);
