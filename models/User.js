d7959d0035c029ff164eda38e0512cc4f2ce1210
minwooKim <vvee1253@gmail.com>
2021-04-16 21:53:48 +0900

    ADD default photo url

--- models/User.js
+++ models/User.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 const findOrCreate = require("mongoose-findorcreate");
 
-// TODO: add schema and fix default-photo-url
-const defaultPhotoURL = "https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331257__340.png"
+// TODO: add schema
+// TODO: 디폴트 이미지 어쩔지 생각해보기
+const defaultPhotoURL = "https://cdn.pixabay.com/photo/2018/04/18/18/56/user-3331257__340.png";
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    required: [true, 'email is required'],
+    required: [true, "email is required"],
   },
   photoURL: {
     type: String,
