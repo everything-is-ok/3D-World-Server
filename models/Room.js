5e214e521e84c7cf414b8c7b4d0f7de93402f3eb
Parksoyoon <maudlinsy@gmail.com>
2021-04-17 21:39:57 +0900

    Add model schemas

--- models/Room.js
+++ models/Room.js
@@ -1,14 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // TODO: add schema
 
 const roomSchema = new mongoose.Schema({
+  items: [
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+        required: true,
+      },
+      position: {
+        type: Array,
+        default: [],
+      },
+      rotation: {
+        type: Array,
+        default: [],
+      },
+      isPlaced: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+  mailBoxId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "MailBox",
+    required: [true, "mailBoxId is required."],
+  },
   ownerId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: [true, 'ownerId is required.'],
-  }
+    ref: "User",
+    required: [true, "ownerId is required."],
+  },
+  ownerName: {
+    type: String,
+    required: [true, "ownerName is required."],
+  },
 }, {
   timestamps: true,
 });
 
-module.exports = mongoose.model('Room', roomSchema);
+module.exports = mongoose.model("Room", roomSchema);
