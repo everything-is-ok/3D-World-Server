55464f663350a2d385a3df2345c005dce21dad87
minwooKim <vvee1253@gmail.com>
2021-04-19 16:39:29 +0900

    ADD getRoomByUserId

--- routes/mailbox.js
+++ routes/mailbox.js
@@ -5,6 +5,7 @@ const deserialize = require("../middlewares/deserialize");
 
 router.get("/", deserialize, mailboxControllers.getMailList);
 router.delete("/", deserialize, mailboxControllers.deleteMailList);
+// TODO: 사용하지 않는다면 추후 삭제
 // router.patch("/read", mailboxControllers.readMail);
 router.post("/mail/:id", deserialize, mailboxControllers.postMail);
 // router.delete("/mail/:id", mailboxControllers.deleteMail);
