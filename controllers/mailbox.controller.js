bea2f442e07890b0efdf4dc286997314292d69c7
minwooKim <vvee1253@gmail.com>
2021-04-19 17:28:01 +0900

    IMPLEMENT poasMail

--- controllers/mailbox.controller.js
+++ controllers/mailbox.controller.js
@@ -1,13 +1,57 @@
-// TODO: Rename function names
+const mongoose = require("mongoose");
+const createError = require("http-errors");
 
-function getMailbox(req, res) {
-  res.status(200).json({
-    data: {
-      // data
-    },
-  });
+const Mailbox = require("../models/Mailbox");
+const Room = require("../models/Room");
 
-  // error handle
+async function getMailList(req, res, next) {
+  if (!req.user) {
+    next(createError(401, "authorization is invalid"));
+    return;
+  }
+
+  const { _id } = req.user;
+
+  try {
+    const mailboxList = await Room.findOne({ ownerId: _id })
+      .populate("mailboxId");
+
+    res.json({
+      ok: true,
+      data: mailboxList,
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-exports.getMailbox = getMailbox;
+async function postMail(req, res, next) {
+  const { _id: sender } = req.user;
+  const { content } = req.body;
+  const { id } = req.params;
+
+  if (!(mongoose.Types.ObjectId.isValid(id))) {
+    next(createError(400, "id of params is invalid"));
+    return;
+  }
+
+  try {
+    const addEmail = { $push: { mails: { content, sender } } };
+    const mailbox = await Mailbox.findByIdAndUpdate(id, addEmail, { new: true });
+
+    if (!mailbox) {
+      next(createError(401, "bad request"));
+      return;
+    }
+
+    res.json({
+      ok: true,
+      data: mailbox,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+exports.getMailList = getMailList;
+exports.postMail = postMail;
