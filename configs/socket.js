5026d1293b59f066dff340d151dc712b383e2953
sungjinsama <a01081440011@gmail.com>
2021-04-21 12:08:16 +0900

    MODIFY get name when user join room

--- configs/socket.js
+++ configs/socket.js
@@ -6,36 +6,36 @@ socketIo.on("connection", (socket) => {
   console.log("A user connected to socket");
 
   socket.on("room", ({ user, roomId }) => {
-    console.log(`${user.name} user join ${roomId}`);
+    console.log(`${user} user join ${roomId}`);
 
     // NOTE: socket.on("room")의 user, roomId와 closure가 형성되어있기때문에, 별도의 룸 관리가 없어도 될 듯
     // TODO: 사용하지 않는다면 추후 삭제
     openedRooms[roomId]
-      ? openedRooms[roomId][socket.id] = user.name
-      : openedRooms[roomId] = { [socket.id]: user.name };
+      ? openedRooms[roomId][socket.id] = user
+      : openedRooms[roomId] = { [socket.id]: user };
 
     console.log("From join room, current opened room list", openedRooms);
 
     socket.join(roomId);
     socket.broadcast
       .to(roomId)
-      .emit("room", { user: user.name });
+      .emit("room", { user });
 
     socket.on("chat", ({ message }) => {
       socket.broadcast
         .to(roomId)
-        .emit("chat", { user: user.name, message });
+        .emit("chat", { user, message });
     });
 
     socket.on("move", ({ position, direction }) => {
-      console.log(`name: ${user.name}, position: ${position}, direction: ${direction}`);
+      console.log(`name: ${user}, position: ${position}, direction: ${direction}`);
       socket.broadcast
         .to(roomId)
-        .emit("move", { user: user.name, position, direction });
+        .emit("move", { user, position, direction });
     });
 
     socket.on("disconnect", () => {
-      console.log(`A ${user.name}user disconnected from socket`);
+      console.log(`A ${user}user disconnected from socket`);
 
       // NOTE: socket.on("room")의 user, roomId와 closure가 형성되어있기때문에, 별도의 룸 관리가 없어도 될 듯
       // TODO: 사용하지 않는다면 추후 삭제
